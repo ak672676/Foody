9a6f6fe94cbbfd9a017ac6ee438ed4149a3060d7
Amit Kumar <46594792+ak672676@users.noreply.github.com>
2022-01-11 13:53:02 +0530

    Navbar done

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+   
       <Head>
         <title>Pizza Resturant</title>
         <meta name="description" content="Best pizza shop"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Image src="/img/pizza.png" alt="pizza" width={400} height={400} />
+
       Homepage
+    
     </div>
   )
 }
