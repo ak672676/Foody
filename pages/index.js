49e4f2954a8c94b5579b474fa07534c9f0a3d297
Amit Kumar <46594792+ak672676@users.noreply.github.com>
2022-01-12 15:38:37 +0530

    PizzaList added

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
-import Head from 'next/head'
-import Featured from '../component/Featured'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Featured from "../component/Featured";
+import PizzaList from "../component/PizzaList";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-   
       <Head>
         <title>Pizza Resturant</title>
-        <meta name="description" content="Best pizza shop"/>
+        <meta name="description" content="Best pizza shop" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <Featured />
-    
+      <Featured />
+      <PizzaList />
     </div>
-  )
+  );
 }
